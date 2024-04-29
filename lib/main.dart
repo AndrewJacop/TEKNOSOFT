2ed0fe1d6f68cd0777a870e76673a4cdb20cb70e
AndrewJacop <andrewjacop@ymail.com>
2024-04-29 21:43:33 +0300

    Initialized Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_t_store/app.dart';
+import 'package:flutter_t_store/firebase_options.dart';
+import 'package:get/get.dart';
 
-void main() {
+Future<void> main() async {
+// Todo: Add Widgets Binding
+// Todo: Init Local Storage
+// Todo: Init Payment Methods
+// Todo: Await Native Splash
+
+// Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+      .then(
+    (FirebaseApp value) => Get.put(AuthenticationRepository()),
+  );
+
+// Todo: Initialize Authentication
+
+// Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
